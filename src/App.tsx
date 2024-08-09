462053c09d2294aa65a35bff69f93278a53a3c67
Binita <binitagyawali000@gmail.com>
2024-08-09 12:59:11 -0500

    add the functionality not showing tab for workflowpage

--- src/App.tsx
+++ src/App.tsx
@@ -21,39 +21,46 @@ import NotFound from "./pages/NotFound";
 import CollectionDetail from "./pages/CollectionDetail";
 import WorkflowPage from "./pages/Workflows/WorkflowPage";
 
+export const App = () => {
+  const workflowsEnabled = process.env.REACT_APP_WORKFLOWS === "True";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <StacApiProvider apiUrl={process.env.REACT_APP_STAC_API!}>
-      <Router>
-        <Container mx="auto" p="5" bgColor="white" boxShadow="md">
-          <Box
-            as="header"
-            borderBottom="1px dashed"
-            borderColor="gray.300"
-            mb="4"
-            pb="4"
-            display="flex"
-          >
-            <Box flex="1" fontWeight="bold" textTransform="uppercase">STAC Admin</Box>
-            <MainNavigation />
-          </Box>
-          <Box as="main">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/collections/" element={<CollectionList />} />
-              <Route path="/collections/new_collection/" element={<CollectionForm />} />
-              <Route path="/collections/:collectionId/" element={<CollectionDetail />} />
-              <Route path="/collections/:collectionId/edit/" element={<CollectionForm />} />
-              <Route path="/items/" element={<ItemList />} />
-              <Route path="/collections/:collectionId/items/:itemId/" element={<ItemDetail />} />
-              <Route path="/collections/:collectionId/items/:itemId/edit/" element={<ItemForm />} />
-              <Route path="/workflows/" element={<WorkflowPage />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Box>
-        </Container>
-      </Router>
-    </StacApiProvider>
-  </ChakraProvider>
-);
+  return (
+    <ChakraProvider theme={theme}>
+      <StacApiProvider apiUrl={process.env.REACT_APP_STAC_API!}>
+        <Router>
+          <Container mx="auto" p="5" bgColor="white" boxShadow="md">
+            <Box
+              as="header"
+              borderBottom="1px dashed"
+              borderColor="gray.300"
+              mb="4"
+              pb="4"
+              display="flex"
+            >
+              <Box flex="1" fontWeight="bold" textTransform="uppercase">
+                STAC Admin
+              </Box>
+              <MainNavigation />
+            </Box>
+            <Box as="main">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/collections/" element={<CollectionList />} />
+                <Route path="/collections/new_collection/" element={<CollectionForm />} />
+                <Route path="/collections/:collectionId/" element={<CollectionDetail />} />
+                <Route path="/collections/:collectionId/edit/" element={<CollectionForm />} />
+                <Route path="/items/" element={<ItemList />} />
+                <Route path="/collections/:collectionId/items/:itemId/" element={<ItemDetail />} />
+                <Route path="/collections/:collectionId/items/:itemId/edit/" element={<ItemForm />} />
+                {workflowsEnabled && (
+                  <Route path="/workflows/" element={<WorkflowPage />} />
+                )}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Box>
+          </Container>
+        </Router>
+      </StacApiProvider>
+    </ChakraProvider>
+  );
+};
--- src/pages/Workflows/WorkflowPage.tsx
+++ src/pages/Workflows/WorkflowPage.tsx
@@ -3,7 +3,7 @@ import {
   Box, Button, Menu, MenuButton, MenuList, MenuItem, Container, Heading, Flex, Text, Input, Table, Tbody, Td, Th, Thead, Tr, IconButton, Textarea, FormControl, FormLabel, FormErrorMessage,
 } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
-import { useForm, useFieldArray, FieldValues } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { MdAdd, MdDelete } from "react-icons/md";
 import { WorkflowFormValues } from "./types"; // Import the WorkflowFormValues type
 
@@ -33,8 +33,6 @@ const WorkflowPage: React.FC = () => {
   const [jsonInput, setJsonInput] = useState("");
   const [jsonError, setJsonError] = useState("");
   const [showForm, setShowForm] = useState(false); // State to control form visibility
-
-  const workflowsEnabled = process.env.REACT_APP_WORKFLOWS === "True"; // Read environment variable
 
   const watchedValues = watch();
 
@@ -99,230 +97,219 @@ const WorkflowPage: React.FC = () => {
   };
 
   return (
-    workflowsEnabled ? (
-      <Container maxW="container.lg" py={8}>
-        <Flex justifyContent="space-between" alignItems="center" mb={6}>
-          <Heading as="h1" size="lg">
-            Workflows
-          </Heading>
-          {showForm && (
-            <Button type="button" onClick={toggleJsonMode}>
-              {isJsonMode ? "Form" : "JSON"}
-            </Button>
-          )}
-        </Flex>
-        <Box
-          bg="gray.50"
-          p={6}
-          borderRadius="md"
-          boxShadow="sm"
-          mb={8}
-          border="1px solid"
-          borderColor="gray.200"
-        >
-          <Text fontSize="lg" mb={4}>
-            Select a workflow:
-          </Text>
-          <Menu>
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-              {selectedOption}
-            </MenuButton>
-            <MenuList>
-              <MenuItem onClick={() => handleOptionSelect("Ingest from S3")}>
-                Ingest from S3
-              </MenuItem>
-              <MenuItem onClick={() => handleOptionSelect("Copy from STAC")}>
-                Copy from STAC
-              </MenuItem>
-              <MenuItem onClick={() => handleOptionSelect("Option C")}>
-                Option C
-              </MenuItem>
-            </MenuList>
-          </Menu>
-        </Box>
-
-        {showForm && (
-          isJsonMode ? (
-            <Box
-              p={6}
-              borderRadius="md"
-              boxShadow="sm"
-              mb={8}
-              border="1px solid"
-              borderColor="gray.200"
-              position="relative"
-            >
-              <Textarea
-                value={jsonInput}
-                onChange={handleJsonChange}
-                placeholder="Edit workflow in JSON format"
-                size="md"
-                minHeight="300px"
-              />
-              {jsonError && <Text color="red.500">{jsonError}</Text>}
-              <Flex justifyContent="flex-end" mt="4">
-                <Button onClick={handleSubmit(() => onSubmit(JSON.parse(jsonInput)))}>
-                  Run Workflow
-                </Button>
-              </Flex>
-            </Box>
-          ) : (
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <FormControl isInvalid={!!errors.id}>
-                <FormLabel>ID</FormLabel>
-                <Input
-                  placeholder="ID"
-                  {...register("id", { required: "Collection ID" })}
-                />
-                <FormErrorMessage>{errors.id && errors.id.message}</FormErrorMessage>
-              </FormControl>
-
-              <FormControl isInvalid={!!errors.bucket} mt={4}>
-                <FormLabel>Bucket</FormLabel>
-                <Input
-                  placeholder="Bucket"
-                  {...register("bucket", { required: "Bucket" })}
-                />
-                <FormErrorMessage>{errors.bucket && errors.bucket.message}</FormErrorMessage>
-              </FormControl>
-
-              <FormControl isInvalid={!!errors.prefix} mt={4}>
-                <FormLabel>Prefix</FormLabel>
-                <Input
-                  placeholder="Prefix"
-                  {...register("prefix", { required: "Prefix" })}
-                />
-                <FormErrorMessage>{errors.prefix && errors.prefix.message}</FormErrorMessage>
-              </FormControl>
-
-              <FormControl isInvalid={!!errors.filename_regex} mt={4}>
-                <FormLabel>Filename Regex</FormLabel>
-                <Input
-                  placeholder="Filename Regex"
-                  {...register("filename_regex", { required: "Filename Regex" })}
-                />
-                <FormErrorMessage>{errors.filename_regex && errors.filename_regex.message}</FormErrorMessage>
-              </FormControl>
-
-              <FormControl isInvalid={!!errors.id_regex} mt={4}>
-                <FormLabel>ID Regex</FormLabel>
-                <Input
-                  placeholder="ID Regex"
-                  {...register("id_regex", { required: "ID Regex" })}
-                />
-                <FormErrorMessage>{errors.id_regex && errors.id_regex.message}</FormErrorMessage>
-              </FormControl>
-
-              <FormControl isInvalid={!!errors.id_template} mt={4}>
-                <FormLabel>ID Template</FormLabel>
-                <Input
-                  placeholder="ID Template"
-                  {...register("id_template", { required: "ID Template" })}
-                />
-                <FormErrorMessage>{errors.id_template && errors.id_template.message}</FormErrorMessage>
-              </FormControl>
-
-              <FormControl isInvalid={!!errors.datetime_range} mt={4}>
-                <FormLabel>Datetime Range</FormLabel>
-                <Input
-                  placeholder="Datetime Range"
-                  {...register("datetime_range", { required: "Datetime Range" })}
-                />
-                <FormErrorMessage>{errors.datetime_range && errors.datetime_range.message}</FormErrorMessage>
-              </FormControl>
-
-              <FormControl isInvalid={!!errors.discovery} mt={4}>
-                <FormLabel>Discovery</FormLabel>
-                <Input
-                  placeholder="Discovery"
-                  {...register("discovery", { required: "Discovery" })}
-                />
-                <FormErrorMessage>{errors.discovery && errors.discovery.message}</FormErrorMessage>
-              </FormControl>
-
-              <FormControl isInvalid={!!errors.upload} mt={4}>
-                <FormLabel>Upload</FormLabel>
-                <Input
-                  placeholder="Upload"
-                  {...register("upload", { required: "Upload" })}
-                />
-                <FormErrorMessage>{errors.upload && errors.upload.message}</FormErrorMessage>
-              </FormControl>
-
-              <fieldset>
-                <legend>Assets</legend>
-                <Table variant="simple" size="sm">
-                  <Thead>
-                    <Tr>
-                      <Th>Title</Th>
-                      <Th>Description</Th>
-                      <Th>Regex</Th>
-                      <Th aria-label="Actions" />
-                    </Tr>
-                  </Thead>
-                  <Tbody>
-                    {fields.map((field, index) => (
-                      <Tr key={field.id}>
-                        <Td>
-                          <Input
-                            {...register(`assets.${index}.title`, { required: "Title is required" })}
-                            placeholder="Title"
-                          />
-                        </Td>
-                        <Td>
-                          <Input
-                            {...register(`assets.${index}.description`, { required: "Description is required" })}
-                            placeholder="Description"
-                          />
-                        </Td>
-                        <Td>
-                          <Input
-                            {...register(`assets.${index}.regex`, { required: "Regex is required" })}
-                            placeholder="Regex"
-                          />
-                        </Td>
-                        <Td>
-                          <IconButton
-                            type="button"
-                            icon={<MdDelete />}
-                            aria-label="Remove asset"
-                            onClick={() => remove(index)}
-                          />
-                        </Td>
-                      </Tr>
-                    ))}
-                  </Tbody>
-                </Table>
-                <Box textAlign="right" mt="2">
-                  <Button
-                    type="button"
-                    variant="link"
-                    leftIcon={<MdAdd />}
-                    onClick={() => append({ title: "", description: "", regex: "" })}
-                  >
-                    Add Asset
-                  </Button>
-                </Box>
-              </fieldset>
-              <Flex justifyContent="flex-end" mt="4">
-                <Button type="submit">
-                  Run Workflow
-                </Button>
-              </Flex>
-            </form>
-          )
-        )}
-      </Container>
-    ) : (
-      <Container maxW="container.lg" py={8}>
+    <Container maxW="container.lg" py={8}>
+      <Flex justifyContent="space-between" alignItems="center" mb={6}>
         <Heading as="h1" size="lg">
-          Workflows Disabled
+          Workflows
         </Heading>
+        {showForm && (
+          <Button type="button" onClick={toggleJsonMode}>
+            {isJsonMode ? "Form" : "JSON"}
+          </Button>
+        )}
+      </Flex>
+      <Box
+        bg="gray.50"
+        p={6}
+        borderRadius="md"
+        boxShadow="sm"
+        mb={8}
+        border="1px solid"
+        borderColor="gray.200"
+      >
         <Text fontSize="lg" mb={4}>
-          Workflows feature is currently disabled.
+          Select a workflow:
         </Text>
-      </Container>
-    )
+        <Menu>
+          <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+            {selectedOption}
+          </MenuButton>
+          <MenuList>
+            <MenuItem onClick={() => handleOptionSelect("Ingest from S3")}>
+              Ingest from S3
+            </MenuItem>
+            <MenuItem onClick={() => handleOptionSelect("Copy from STAC")}>
+              Copy from STAC
+            </MenuItem>
+            <MenuItem onClick={() => handleOptionSelect("Option C")}>
+              Option C
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </Box>
+
+      {showForm && (
+        isJsonMode ? (
+          <Box
+            p={6}
+            borderRadius="md"
+            boxShadow="sm"
+            mb={8}
+            border="1px solid"
+            borderColor="gray.200"
+            position="relative"
+          >
+            <Textarea
+              value={jsonInput}
+              onChange={handleJsonChange}
+              placeholder="Edit workflow in JSON format"
+              size="md"
+              minHeight="430px"
+            />
+            {jsonError && <Text color="red.500">{jsonError}</Text>}
+            <Flex justifyContent="flex-end" mt="4">
+              <Button onClick={handleSubmit(() => onSubmit(JSON.parse(jsonInput)))}>
+                Run Workflow
+              </Button>
+            </Flex>
+          </Box>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl isInvalid={!!errors.id}>
+              <FormLabel>ID</FormLabel>
+              <Input
+                placeholder="ID"
+                {...register("id", { required: "Collection ID" })}
+              />
+              <FormErrorMessage>{errors.id && errors.id.message}</FormErrorMessage>
+            </FormControl>
+
+            <FormControl isInvalid={!!errors.bucket} mt={4}>
+              <FormLabel>Bucket</FormLabel>
+              <Input
+                placeholder="Bucket"
+                {...register("bucket", { required: "Bucket" })}
+              />
+              <FormErrorMessage>{errors.bucket && errors.bucket.message}</FormErrorMessage>
+            </FormControl>
+
+            <FormControl isInvalid={!!errors.prefix} mt={4}>
+              <FormLabel>Prefix</FormLabel>
+              <Input
+                placeholder="Prefix"
+                {...register("prefix", { required: "Prefix" })}
+              />
+              <FormErrorMessage>{errors.prefix && errors.prefix.message}</FormErrorMessage>
+            </FormControl>
+
+            <FormControl isInvalid={!!errors.filename_regex} mt={4}>
+              <FormLabel>Filename Regex</FormLabel>
+              <Input
+                placeholder="Filename Regex"
+                {...register("filename_regex", { required: "Filename Regex" })}
+              />
+              <FormErrorMessage>{errors.filename_regex && errors.filename_regex.message}</FormErrorMessage>
+            </FormControl>
+
+            <FormControl isInvalid={!!errors.id_regex} mt={4}>
+              <FormLabel>ID Regex</FormLabel>
+              <Input
+                placeholder="ID Regex"
+                {...register("id_regex", { required: "ID Regex" })}
+              />
+              <FormErrorMessage>{errors.id_regex && errors.id_regex.message}</FormErrorMessage>
+            </FormControl>
+
+            <FormControl isInvalid={!!errors.id_template} mt={4}>
+              <FormLabel>ID Template</FormLabel>
+              <Input
+                placeholder="ID Template"
+                {...register("id_template", { required: "ID Template" })}
+              />
+              <FormErrorMessage>{errors.id_template && errors.id_template.message}</FormErrorMessage>
+            </FormControl>
+
+            <FormControl isInvalid={!!errors.datetime_range} mt={4}>
+              <FormLabel>Datetime Range</FormLabel>
+              <Input
+                placeholder="Datetime Range"
+                {...register("datetime_range", { required: "Datetime Range" })}
+              />
+              <FormErrorMessage>{errors.datetime_range && errors.datetime_range.message}</FormErrorMessage>
+            </FormControl>
+
+            <FormControl isInvalid={!!errors.discovery} mt={4}>
+              <FormLabel>Discovery</FormLabel>
+              <Input
+                placeholder="Discovery"
+                {...register("discovery", { required: "Discovery" })}
+              />
+              <FormErrorMessage>{errors.discovery && errors.discovery.message}</FormErrorMessage>
+            </FormControl>
+
+            <FormControl isInvalid={!!errors.upload} mt={4}>
+              <FormLabel>Upload</FormLabel>
+              <Input
+                placeholder="Upload"
+                {...register("upload", { required: "Upload" })}
+              />
+              <FormErrorMessage>{errors.upload && errors.upload.message}</FormErrorMessage>
+            </FormControl>
+
+            <fieldset>
+              <legend>Assets</legend>
+              <Table variant="simple" size="sm">
+                <Thead>
+                  <Tr>
+                    <Th>Title</Th>
+                    <Th>Description</Th>
+                    <Th>Regex</Th>
+                    <Th aria-label="Actions" />
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {fields.map((field, index) => (
+                    <Tr key={field.id}>
+                      <Td>
+                        <Input
+                          {...register(`assets.${index}.title`, { required: "Title is required" })}
+                          placeholder="Title"
+                        />
+                      </Td>
+                      <Td>
+                        <Input
+                          {...register(`assets.${index}.description`, { required: "Description is required" })}
+                          placeholder="Description"
+                        />
+                      </Td>
+                      <Td>
+                        <Input
+                          {...register(`assets.${index}.regex`, { required: "Regex is required" })}
+                          placeholder="Regex"
+                        />
+                      </Td>
+                      <Td>
+                        <IconButton
+                          type="button"
+                          icon={<MdDelete />}
+                          aria-label="Remove asset"
+                          onClick={() => remove(index)}
+                        />
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+              <Box textAlign="right" mt="2">
+                <Button
+                  type="button"
+                  variant="link"
+                  leftIcon={<MdAdd />}
+                  onClick={() => append({ title: "", description: "", regex: "" })}
+                >
+                  Add Asset
+                </Button>
+              </Box>
+            </fieldset>
+            <Flex justifyContent="flex-end" mt="4">
+              <Button type="submit">
+                Run Workflow
+              </Button>
+            </Flex>
+          </form>
+        )
+      )}
+    </Container>
   );
 };
 
