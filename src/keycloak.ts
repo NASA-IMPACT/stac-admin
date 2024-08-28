e1adfa500d9180dc0ef9ed00a37bb0f180f12229
Binita <binitagyawali000@gmail.com>
2024-08-28 09:32:04 -0500

    change the name of env variable

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -3,6 +3,6 @@ import Keycloak from "keycloak-js";
 
 export const keycloakInstance = new Keycloak({
   realm: process.env.REACT_APP_KEYCLOAK_REALM,
-  url: process.env.REACT_APP_KEYCLOAK_URL,
+  url: process.env.REACT_APP_KEYCLOAK_WORKFLOWS_API,
   clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID,
 });
