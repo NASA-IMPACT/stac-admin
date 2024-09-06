19b47f1be719118b5a21bab7770702c353e29da9
Binita <binitagyawali000@gmail.com>
2024-09-06 08:51:37 -0500

    clean code

--- src/pages/sorryPage.tsx
+++ src/pages/sorryPage.tsx
@@ -3,19 +3,19 @@ import { useLocation } from "react-router-dom";
 
 function SorryPage() {
   const location = useLocation();
-  const type = location.state?.type || "collection";  // Default to 'collection' if type is not provided
+  const type = location.state?.type || "collection";
 
   const isCollection = type === "collection";
 
   return (
     <Box textAlign="center" py={10} px={6}>
       <Heading as="h2" size="xl" mb={4}>
-        {isCollection ? "Sorry, this is not a valid collection." : "Sorry, this is not a valid item."}
+        {isCollection ? "Sorry, this is not a valid item." : "Sorry, this is not a valid collection."}
       </Heading>
       <Text color="gray.500">
         {isCollection 
-          ? "The collection you are looking for does not exist or is invalid."
-          : "The item you are looking for does not exist or is invalid."}
+          ? "The item you are looking for does not exist or is invalid."
+          : "The collection you are looking for does not exist or is invalid."}
       </Text>
     </Box>
   );
