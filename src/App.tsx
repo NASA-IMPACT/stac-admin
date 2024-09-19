02ba602667f6b07736b567f94924b23cbf4de39b
Slesa Adhikari <slesaad@gmail.com>
2024-09-19 09:15:40 -0500

    Add authentication and login page

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,11 @@ import {
 import {
   ChakraProvider,
   Box,
+  Button,
   Container,
 } from "@chakra-ui/react";
+import { ArrowRightIcon } from "@chakra-ui/icons";
+import { useAuth } from "react-oidc-context";
 import { StacApiProvider } from "@developmentseed/stac-react";
 import theme from "./theme";
 import { MainNavigation } from "./components";
@@ -27,6 +30,7 @@ import ErrorBoundary from "./components/ErrorBoundary";
 export const App = () => {
   const workflowsEnabled = process.env.REACT_APP_WORKFLOWS === "True";
   const apiUrl = process.env.REACT_APP_STAC_API;
+  const auth = useAuth();
 
   if (!apiUrl) {
     throw new Error("REACT_APP_STAC_API is not defined");
@@ -37,37 +41,66 @@ export const App = () => {
       <StacApiProvider apiUrl={apiUrl}>
         <Router>
           <ErrorBoundary>
-            <Container mx="auto" p="5" bgColor="white" boxShadow="md">
-              <Box
-                as="header"
-                borderBottom="1px dashed"
-                borderColor="gray.300"
-                mb="4"
-                pb="4"
-                display="flex"
-              >
-                <Box flex="1" fontWeight="bold" textTransform="uppercase">STAC Admin</Box>
-                <MainNavigation />
-              </Box>
-              <Box as="main">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/collections/" element={<CollectionList />} />
-                  <Route path="/collections/new_collection/" element={<CollectionForm />} />
-                  <Route path="/collections/:collectionId/" element={<CollectionDetail />} />
-                  <Route path="/collections/:collectionId/edit/" element={<CollectionForm />} />
-                  <Route path="/items/" element={<ItemList />} />
-                  <Route path="items/new_item/" element={<ItemForm />} />
-                  <Route path="/success" element={<SuccessPage />} />
-                  <Route path="/sorry" element={<SorryPage />} />
-                  <Route path="/collections/:collectionId/items/:itemId/" element={<ItemDetail />} />
-                  <Route path="/collections/:collectionId/items/:itemId/edit/" element={<ItemForm />} />
-                  {workflowsEnabled && (
-                    <Route path="/workflows/" element={<WorkflowPage />} />
-                  )}
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </Box>
+            <Container height="100vh" mx="auto" p="5" bgColor="white" boxShadow="md">
+              {
+
+                auth.isLoading &&
+                <div>Loading...</div>
+              }
+
+              {                
+                auth.error &&
+                <div>Oops... {auth.error.message}</div>
+              }
+
+              {                
+                auth.isAuthenticated && (!(auth.isLoading || auth.error) ) &&
+                (
+                  <>
+                    <Box
+                      as="header"
+                      borderBottom="1px dashed"
+                      borderColor="gray.300"
+                      mb="4"
+                      pb="4"
+                      display="flex"
+                    >
+                      <Box flex="1" fontWeight="bold" textTransform="uppercase">STAC Admin</Box>
+                      <MainNavigation />
+                    </Box>
+                    <Box as="main">
+                      <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/collections/" element={<CollectionList />} />
+                        <Route path="/collections/new_collection/" element={<CollectionForm />} />
+                        <Route path="/collections/:collectionId/" element={<CollectionDetail />} />
+                        <Route path="/collections/:collectionId/edit/" element={<CollectionForm />} />
+                        <Route path="/items/" element={<ItemList />} />
+                        <Route path="items/new_item/" element={<ItemForm />} />
+                        <Route path="/success" element={<SuccessPage />} />
+                        <Route path="/sorry" element={<SorryPage />} />
+                        <Route path="/collections/:collectionId/items/:itemId/" element={<ItemDetail />} />
+                        <Route path="/collections/:collectionId/items/:itemId/edit/" element={<ItemForm />} />
+                        {workflowsEnabled && (
+                          <Route path="/workflows/" element={<WorkflowPage />} />
+                        )}
+                        <Route path="*" element={<NotFound />} />
+                      </Routes>
+                    </Box>
+                  </>
+                )
+              }
+              {
+                !auth.isAuthenticated && (!(auth.isLoading || auth.error) ) &&(
+                  <Button
+                    type="submit"
+                    onClick={() => auth.signinRedirect()}
+                    rightIcon={<ArrowRightIcon />}
+                  >
+                    Log in
+                  </Button>
+                )
+              }
             </Container>
           </ErrorBoundary>
         </Router>
--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,55 @@ import * as ReactDOM from "react-dom/client";
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
-
+import { AuthProvider, AuthProviderProps } from "react-oidc-context";
+import { WebStorageStateStore } from "oidc-client-ts";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(container);
 
-root.render(
-  <React.StrictMode>
-    <ColorModeScript />
-    <App />
-  </React.StrictMode>,
-);
+const authority = `${process.env.REACT_APP_KEYCLOAK_URL}/identity-management/`;
+const wellKnownEndpoint = `${authority}tenant/${process.env.REACT_APP_KEYCLOAK_REALM}/.well-known/openid-configuration`;
+
+async function getOidcConfig() {
+  const response = await fetch(wellKnownEndpoint);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch OIDC config from ${wellKnownEndpoint}`);
+  }
+  return await response.json();
+}
+
+getOidcConfig().then(oidcConfig => {
+  const theConfig: AuthProviderProps = {
+    authority: `${authority}`,
+    client_id: process.env.REACT_APP_KEYCLOAK_CLIENT_ID || "",
+    redirect_uri: "https://dojxsily14em4.cloudfront.net",
+    // redirect_uri: "http://localhost:3000",
+    response_type: "code",
+    scope: "openid email",
+    metadata: {
+      authorization_endpoint: oidcConfig.authorization_endpoint,
+      token_endpoint: oidcConfig.token_endpoint,
+      revocation_endpoint: oidcConfig.revocation_endpoint,
+      introspection_endpoint: oidcConfig.introspection_endpoint,
+      userinfo_endpoint: oidcConfig.userinfo_endpoint,
+      jwks_uri: oidcConfig.jwks_uri,
+    },
+    userStore: new WebStorageStateStore({ store: window.localStorage }),
+    automaticSilentRenew: true,
+  };
+
+  root.render(
+    <React.StrictMode>
+      <ColorModeScript />
+      <AuthProvider {...theConfig}>
+        <App />
+      </AuthProvider>
+    </React.StrictMode>,
+  );
+}).catch((error) => {
+  console.error("Error loading OIDC configuration", error);
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
@@ -26,4 +63,3 @@ serviceWorker.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
