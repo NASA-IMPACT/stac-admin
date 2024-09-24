54edecbfd039156f4115eef82d51a69974f91212
Binita <binitagyawali000@gmail.com>
2024-09-24 09:54:24 -0500

    worked on datetime format warning

--- src/pages/ItemForm/index.tsx
+++ src/pages/ItemForm/index.tsx
@@ -23,7 +23,7 @@ import {
   AlertDescription,
   AlertTitle,
   CloseButton,
-  AlertIcon
+  AlertIcon,
 } from "@chakra-ui/react";
 import { MdDelete, MdAdd } from "react-icons/md";
 import { FormValues } from "./types";
@@ -113,6 +113,29 @@ const defaultValues: FormValues = {
   },
 };
 
+// Utility function to format datetime strings
+const formatDatetime = (datetime: string) => {
+  if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/.test(datetime)) {
+    // Format yyyy-MM-ddThh:mm -> yyyy-MM-ddThh:mm:00Z
+    return `${datetime}:00Z`;
+  } else if (/^\d{4}-\d{2}-\d{2}T\d{2}$/.test(datetime)) {
+    // Format yyyy-MM-ddThh -> yyyy-MM-ddThh:00:00Z
+    return `${datetime}:00:00Z`;
+  } else if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/.test(datetime)) {
+    // Format yyyy-MM-ddThh:mm:ss -> yyyy-MM-ddThh:mm:ssZ
+    return `${datetime}Z`;
+  } else if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}$/.test(datetime)) {
+    // Format yyyy-MM-ddThh:mm:ss.SSS -> yyyy-MM-ddThh:mm:ss.SSSZ
+    return `${datetime}Z`;
+  }
+  return datetime; // If already in correct format, return as is
+};
+
+// Function to format datetime for input fields
+const formatDatetimeForInput = (datetime: string) => {
+  return formatDatetime(datetime).replace(/([+-]\d{2}:\d{2}|Z)$/, "");
+};
+
 export default function ItemForm() {
   const { collectionId, itemId } = useParams();
   const isNewItem = !itemId;
@@ -134,7 +157,6 @@ export default function ItemForm() {
     }
   }, [location.state]);
 
-
   const [selectedCollectionId, setSelectedCollectionId] = useState<string>(collectionId || "");
   const [isJsonMode, setJsonMode] = useState(false);
   const [jsonInput, setJsonInput] = useState("");
@@ -154,7 +176,7 @@ export default function ItemForm() {
     defaultValues: isNewItem ? defaultValues : (item as FormValues),
     values: !isNewItem ? (item as FormValues) : undefined,
   });
- 
+
   const { fields, append, remove } = useFieldArray({
     control,
     name: "properties.providers",
@@ -184,21 +206,19 @@ export default function ItemForm() {
   const handleRangeUpdate = (v?: string) => {
     if (v) {
       setValue("properties.datetime", null);
-      return v.endsWith("Z") ? v : `${v}:00Z`;
+      return formatDatetime(v); // Use utility function to format
     }
     return v;
   };
-  
+
   const handleSingleDateUpdate = (v?: string) => {
     if (v) {
       setValue("properties.start_datetime", undefined);
       setValue("properties.end_datetime", undefined);
-      return v.endsWith("Z") ? v : `${v}:00Z`;
+      return formatDatetime(v); // Use utility function to format
     }
     return null;
   };
-  
-
 
   useEffect(() => {
     if (!item || isNewItem) return;
@@ -206,11 +226,11 @@ export default function ItemForm() {
     const { start_datetime, end_datetime, datetime } = item.properties;
     if (start_datetime && end_datetime) {
       setDateType("range");
-      setValue("properties.start_datetime", start_datetime.split("Z")[0]);
-      setValue("properties.end_datetime", end_datetime.split("Z")[0]);
+      setValue("properties.start_datetime", formatDatetimeForInput(start_datetime));
+      setValue("properties.end_datetime", formatDatetimeForInput(end_datetime));
     } else {
       setDateType("single");
-      setValue("properties.datetime", datetime.split("Z")[0]);
+      setValue("properties.datetime", formatDatetimeForInput(datetime));
     }
   }, [item, setValue, isNewItem]);
 
@@ -218,7 +238,18 @@ export default function ItemForm() {
     setSuccessMessage("");
     setErrorMessage("");
 
-    const itemId = data.id;
+    // Format datetime fields for submission
+    const formattedData = {
+      ...data,
+      properties: {
+        ...data.properties,
+        datetime: data.properties.datetime ? formatDatetime(data.properties.datetime) : null,
+        start_datetime: data.properties.start_datetime ? formatDatetime(data.properties.start_datetime) : null,
+        end_datetime: data.properties.end_datetime ? formatDatetime(data.properties.end_datetime) : null,
+      },
+    };
+
+    const itemId = formattedData.id;
     try {
       if (isNewItem) {
         const postUrl = `${process.env.REACT_APP_STAC_API}/collections/${selectedCollectionId}/items`;
@@ -226,7 +257,7 @@ export default function ItemForm() {
         await Api.fetch(postUrl, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(data),
+          body: JSON.stringify(formattedData),
         });
         navigate("/success", {
           state: {
@@ -237,8 +268,8 @@ export default function ItemForm() {
           },
         });
       } else {
-        const coercedData = data as StacItem;
-        await update(coercedData); 
+        const coercedData = formattedData as StacItem;
+        await update(coercedData);
         navigate("/success", {
           state: {
             successMessage: `Successfully updated the item ${itemId}.`,
@@ -255,7 +286,7 @@ export default function ItemForm() {
         const errorDetails = apiError.detail;
         const action = isNewItem ? "creating" : "editing";
         if (errorDetails.code && errorDetails.description) {
-          setErrorMessage (
+          setErrorMessage(
             <Box>
               <Text fontWeight="bold">Detail: {errorDetails.code}</Text>
               <Text fontWeight="bold">Description: Validation failed for item with ID {itemId || "Unknown"} while {action} it.</Text>
@@ -289,7 +320,7 @@ export default function ItemForm() {
             </Box>
           );
         } else if (errorDetails.detail) {
-          setErrorMessage (
+          setErrorMessage(
             <Box>
               <Text fontWeight="bold">
                 Validation failed for item with ID {itemId || "Unknown"} while {action} it.
@@ -327,7 +358,7 @@ export default function ItemForm() {
     setJsonMode(!isJsonMode);
     if (!isJsonMode) {
       setJsonInput(JSON.stringify({ ...watchedValues }, null, 2));
-    }else {
+    } else {
       setSelectedCollectionId(watchedValues.collection || "");
       setValue("properties.license", watchedValues.properties?.license || "");
     }
@@ -355,10 +386,7 @@ export default function ItemForm() {
             <AlertDescription>
               {successMessage}{" "}
               <Box mt="2">
-                <Button
-                  variant="link"
-                  onClick={() => window.location.reload()}
-                >
+                <Button variant="link" onClick={() => window.location.reload()}>
                   Create another item
                 </Button>
               </Box>
@@ -381,9 +409,7 @@ export default function ItemForm() {
 
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Text as="h1">
-          <HeadingLead>
-            {isNewItem ? "Add New Item" : `Edit Item ${item?.id}`}
-          </HeadingLead>
+          <HeadingLead>{isNewItem ? "Add New Item" : `Edit Item ${item?.id}`}</HeadingLead>
         </Text>
         <Button type="button" onClick={toggleJsonMode}>
           {isJsonMode ? "Form" : "JSON"}
@@ -392,19 +418,10 @@ export default function ItemForm() {
 
       {isJsonMode ? (
         <Box>
-          <Textarea
-            value={jsonInput}
-            onChange={handleJsonChange}
-            placeholder="Enter item in JSON format"
-            size="md"
-            minHeight="80vh"
-          />
+          <Textarea value={jsonInput} onChange={handleJsonChange} placeholder="Enter item in JSON format" size="md" minHeight="80vh" />
           {jsonError && <Text color="red.500">{jsonError}</Text>}
           <Box mt="4">
-            <Button
-              onClick={handleSubmit(() => onSubmit(JSON.parse(jsonInput)))}
-              isLoading={updateState === "LOADING"}
-            >
+            <Button onClick={handleSubmit(() => onSubmit(JSON.parse(jsonInput)))} isLoading={updateState === "LOADING"}>
               {isNewItem ? "Create Item" : "Save Item"}
             </Button>
           </Box>
@@ -412,17 +429,9 @@ export default function ItemForm() {
       ) : (
         <form onSubmit={handleSubmit(onSubmit)}>
           <Text as="h2">Common Meta Data</Text>
-          <TextInput
-            label="Title"
-            error={errors.properties?.title}
-            {...register("properties.title")}
-          />
-          <TextAreaInput
-            label="Description"
-            error={errors.properties?.description}
-            {...register("properties.description")}
-          />
-          
+          <TextInput label="Title" error={errors.properties?.title} {...register("properties.title")} />
+          <TextAreaInput label="Description" error={errors.properties?.description} {...register("properties.description")} />
+
           <Box>
             <label htmlFor="license">License</label>
             <Controller
@@ -431,7 +440,9 @@ export default function ItemForm() {
               defaultValue=""
               render={({ field }) => (
                 <Select {...field} id="license" overflowY="auto" value={field.value as string | undefined}>
-                  <option value="" disabled>Select a license</option>
+                  <option value="" disabled>
+                    Select a license
+                  </option>
                   {licenses.map((license) => (
                     <option key={license.licenseId} value={license.licenseId}>
                       {license.licenseId} - {license.name}
@@ -452,67 +463,43 @@ export default function ItemForm() {
                 <Radio value="range">Date range</Radio>
               </Stack>
             </RadioGroup>
-            <Box
-              aria-hidden={dateType !== "single"}
-              display={dateType === "single" ? "block" : "none"}
-            >
-              <DateTimeInput  
+            <Box aria-hidden={dateType !== "single"} display={dateType === "single" ? "block" : "none"}>
+              <DateTimeInput
                 label="Enter date"
                 error={errors.properties?.datetime}
                 {...register("properties.datetime", {
-                  setValueAs: handleSingleDateUpdate
+                  setValueAs: handleSingleDateUpdate,
                 })}
               />
             </Box>
-            <Box
-              aria-hidden={dateType === "single"}
-              display={dateType !== "single" ? "flex" : "none"}
-              gap="4"
-            >
+            <Box aria-hidden={dateType === "single"} display={dateType !== "single" ? "flex" : "none"} gap="4">
               <DateTimeInput
                 label="Date/time from"
                 error={errors.properties?.start_datetime}
                 {...register("properties.start_datetime", {
-                  setValueAs: handleRangeUpdate
+                  setValueAs: handleRangeUpdate,
                 })}
               />
               <DateTimeInput
                 label="Date/time to"
                 error={errors.properties?.end_datetime}
                 {...register("properties.end_datetime", {
-                  setValueAs: handleRangeUpdate
+                  setValueAs: handleRangeUpdate,
                 })}
               />
             </Box>
           </fieldset>
-          
-                 
+
           {/* BBox Input */}
           <Box mt={4}>
             <Text as="h2" fontWeight="bold">
               Bounding Box (Bbox)
             </Text>
             <Box display="flex" gap="4" mt="2">
-              <NumberInput
-                label="Min X"
-                error={errors.bbox?.[0]}
-                {...register("bbox.0", { required: "Min X is required" })}
-              />
-              <NumberInput
-                label="Min Y"
-                error={errors.bbox?.[1]}
-                {...register("bbox.1", { required: "Min Y is required" })}
-              />
-              <NumberInput
-                label="Max X"
-                error={errors.bbox?.[2]}
-                {...register("bbox.2", { required: "Max X is required" })}
-              />
-              <NumberInput
-                label="Max Y"
-                error={errors.bbox?.[3]}
-                {...register("bbox.3", { required: "Max Y is required" })}
-              />
+              <NumberInput label="Min X" error={errors.bbox?.[0]} {...register("bbox.0", { required: "Min X is required" })} />
+              <NumberInput label="Min Y" error={errors.bbox?.[1]} {...register("bbox.1", { required: "Min Y is required" })} />
+              <NumberInput label="Max X" error={errors.bbox?.[2]} {...register("bbox.2", { required: "Max X is required" })} />
+              <NumberInput label="Max Y" error={errors.bbox?.[3]} {...register("bbox.3", { required: "Max Y is required" })} />
             </Box>
           </Box>
 
@@ -533,16 +520,10 @@ export default function ItemForm() {
                 {fields.map(({ id }, idx: number) => (
                   <Tr key={id}>
                     <Td>
-                      <Input
-                        {...register(`properties.providers.${idx}.name`)}
-                        aria-labelledby="provider_name"
-                      />
+                      <Input {...register(`properties.providers.${idx}.name`)} aria-labelledby="provider_name" />
                     </Td>
                     <Td>
-                      <Input
-                        {...register(`properties.providers.${idx}.description`)}
-                        aria-labelledby="provider_description"
-                      />
+                      <Input {...register(`properties.providers.${idx}.description`)} aria-labelledby="provider_description" />
                     </Td>
                     <Td>
                       <Controller
@@ -563,31 +544,17 @@ export default function ItemForm() {
                       />
                     </Td>
                     <Td>
-                      <Input
-                        {...register(`properties.providers.${idx}.url`)}
-                        aria-labelledby="provider_url"
-                      />
+                      <Input {...register(`properties.providers.${idx}.url`)} aria-labelledby="provider_url" />
                     </Td>
                     <Td>
-                      <IconButton
-                        type="button"
-                        size="sm"
-                        icon={<MdDelete />}
-                        onClick={() => remove(idx)}
-                        aria-label="Remove provider"
-                      />
+                      <IconButton type="button" size="sm" icon={<MdDelete />} onClick={() => remove(idx)} aria-label="Remove provider" />
                     </Td>
                   </Tr>
                 ))}
               </Tbody>
             </Table>
             <Box textAlign="right" mt="2">
-              <Button
-                type="button"
-                variant="link"
-                leftIcon={<MdAdd />}
-                onClick={() => append({ name: "" })}
-              >
+              <Button type="button" variant="link" leftIcon={<MdAdd />} onClick={() => append({ name: "" })}>
                 Add provider
               </Button>
             </Box>
@@ -596,21 +563,12 @@ export default function ItemForm() {
           {isNewItem && (
             <>
               <Text as="h2">Item Meta Data</Text>
-              <TextInput
-                label="Item ID"
-                error={errors.id}
-                {...register("id", { required: "Item ID is required" })}
-              />
+              <TextInput label="Item ID" error={errors.id} {...register("id", { required: "Item ID is required" })} />
               <Box mt={4}>
                 <Text as="label" fontWeight="bold">
                   Select Collection
                 </Text>
-                <Select
-                  placeholder="Select collection"
-                  value={selectedCollectionId}
-                  onChange={(e) => setSelectedCollectionId(e.target.value)}
-                  isRequired
-                >
+                <Select placeholder="Select collection" value={selectedCollectionId} onChange={(e) => setSelectedCollectionId(e.target.value)} isRequired>
                   {collections?.collections.map((col: StacCollection) => (
                     <option key={col.id} value={col.id}>
                       {col.id}
@@ -619,20 +577,12 @@ export default function ItemForm() {
                 </Select>
               </Box>
 
-              <TextInput
-                label="STAC Version"
-                error={errors.stac_version}
-                {...register("stac_version")}
-              />
+              <TextInput label="STAC Version" error={errors.stac_version} {...register("stac_version")} />
             </>
           )}
 
           <Text as="h2">Instruments</Text>
-          <TextInput
-            label="Platform"
-            error={errors.properties?.platform}
-            {...register("properties.platform")}
-          />
+          <TextInput label="Platform" error={errors.properties?.platform} {...register("properties.platform")} />
           <Controller
             name="properties.instruments"
             render={({ field }) => (
@@ -645,21 +595,9 @@ export default function ItemForm() {
             )}
             control={control}
           />
-          <TextInput
-            label="Constellation"
-            error={errors.properties?.constellation}
-            {...register("properties.constellation")}
-          />
-          <TextInput
-            label="Mission"
-            error={errors.properties?.mission}
-            {...register("properties.mission")}
-          />
-          <NumberInput
-            label="Ground Sample Distance"
-            error={errors.properties?.gsd}
-            {...register("properties.gsd", { min: 1 })}
-          />
+          <TextInput label="Constellation" error={errors.properties?.constellation} {...register("properties.constellation")} />
+          <TextInput label="Mission" error={errors.properties?.mission} {...register("properties.mission")} />
+          <NumberInput label="Ground Sample Distance" error={errors.properties?.gsd} {...register("properties.gsd", { min: 1 })} />
 
           <Box mt="4">
             <Button type="submit" isLoading={updateState === "LOADING"}>
